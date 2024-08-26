7a14a2e887e569a89e966aae7dd806121cac6086
Rohit Yadav <rohit@nimbbl.biz>
2024-08-27 00:15:35 +0530

    creat a first api from dummy json

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
+let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'hello world', status: 'success' });
+app.get('/api/v1/movies', (req, res) => {
+    res.status(200).json({
+        status: "success",
+        data : {
+            movies: movies
+        }
+    })
 })
